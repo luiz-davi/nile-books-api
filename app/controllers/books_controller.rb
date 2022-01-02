7d235c19ae10bfe7be66f303481333fa95bcab87
Luiz Davi <55983920+luiz-davi@users.noreply.github.com>
2022-01-02 15:59:34 -0300

    Endpoint de remoção

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,25 @@ class BooksController < ApplicationController
   def index
     render json: Book.all
   end
+
+  def create
+    book = Book.new(book_params)
+    if book.save
+      render json: book, status: :created
+    else
+      render json: book, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    Book.find(params[:id]).destroy!
+
+    head :no_content
+  end
+
+  private 
+
+    def book_params
+      params.require(:book).permit(:title, :author)
+    end
 end
